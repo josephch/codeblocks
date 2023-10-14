fec88accecc4d4959f3c32ac7655ab291af97206
Christo Joseph <1538412+josephch@users.noreply.github.com>
2023-10-14 09:12:00 +0530

    clangd_client : don't pause parsing of Makefile projects

--- src/plugins/contrib/clangd_client/src/codecompletion/codecompletion.cpp
+++ src/plugins/contrib/clangd_client/src/codecompletion/codecompletion.cpp
@@ -3132,6 +3132,7 @@ void ClgdCompletion::OnWorkspaceChanged(CodeBlocksEvent& event)
                 if (pParser and (not pParser->GetLSPClient()) )
                     LSPsucceeded = GetParseManager()->CreateNewLanguageServiceProcess(pActiveProject, LSPeventID);
 
+#ifdef DEFER_PARSING_FOR_MAKEFILE_PROJECTS
                 // Pause parsing if this is a makefile project //(ph 2023/09/03)
                 if ( pParser and LSPsucceeded and pActiveProject->IsMakefileCustom())
                 {
@@ -3140,6 +3141,7 @@ void ClgdCompletion::OnWorkspaceChanged(CodeBlocksEvent& event)
                     cbPlugin* pPlgn = Manager::Get()->GetPluginManager()->FindPluginByName("clangd_client");
                     if (pPlgn) pPlgn->ProcessEvent(evt);
                 }
+#endif// DEFER_PARSING_FOR_MAKEFILE_PROJECTS
 
             }//if not parser
 
@@ -3284,6 +3286,7 @@ void ClgdCompletion::OnProjectActivated(CodeBlocksEvent& event)
             if (pParser and pParser->PauseParsingCount("Deactivated"))
                 pParser->PauseParsingForReason("Deactivated",false);
         }
+#ifdef DEFER_PARSING_FOR_MAKEFILE_PROJECTS
         // Pause parsing if this is a makefile project //(ph 2023/09/03)
         if (m_CurrProject->IsMakefileCustom())
         {
@@ -3292,6 +3295,7 @@ void ClgdCompletion::OnProjectActivated(CodeBlocksEvent& event)
             cbPlugin* pPlgn = Manager::Get()->GetPluginManager()->FindPluginByName("clangd_client");
             if (pPlgn) pPlgn->ProcessEvent(evt);
         }
+#endif // DEFER_PARSING_FOR_MAKEFILE_PROJECTS
 
     }//endif attached
 
