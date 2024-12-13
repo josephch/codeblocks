495e30e9e59f3aebed3d53cbc8b1b05914e4e942
Christo Joseph <1538412+josephch@users.noreply.github.com>
2024-12-13 22:27:58 +0530

    clangd_client  nrvo fixes

--- src/plugins/contrib/clangd_client/src/codecompletion/codecompletion.cpp
+++ src/plugins/contrib/clangd_client/src/codecompletion/codecompletion.cpp
@@ -3497,9 +3497,7 @@ wxString ClgdCompletion::GetFilenameFromLSP_Response(wxCommandEvent& event)
 
     if (URI.empty()) return wxString();
     wxFileName fnFilename = fileUtils.FilePathFromURI(URI);
-    wxString filename = fnFilename.GetFullPath();
-    if (filename.Length()) return filename;
-    return wxString();
+    return fnFilename.GetFullPath();
 }
 // ----------------------------------------------------------------------------
 void ClgdCompletion::OnLSP_Event(wxCommandEvent& event)
@@ -5669,7 +5667,7 @@ wxString ClgdCompletion::GetTargetsOutFilename(cbProject* pProject)
             if (tgtIdx == -1)
             {
                 //-Log(_("canceled"));
-                return wxString();
+                return buildOutputFile;
             }
             pTarget = pProject->GetBuildTarget(tgtIdx);
             activeBuildTarget = (pTarget ? pTarget->GetTitle() : wxString(wxEmptyString));
@@ -5683,7 +5681,7 @@ wxString ClgdCompletion::GetTargetsOutFilename(cbProject* pProject)
             //cbMessageBox(_("The selected target is only running pre/post build step commands\n"
             //               "Can't debug such a target..."), _("Information"), wxICON_INFORMATION);
             //Log(_("aborted"));
-            return wxString();
+            return buildOutputFile;
         }
         //-if (target) Log(target->GetTitle());
 
@@ -5691,12 +5689,7 @@ wxString ClgdCompletion::GetTargetsOutFilename(cbProject* pProject)
             buildOutputFile = pTarget->GetOutputFilename();
     }
 
-    if (buildOutputFile.Length())
-    {
-        return buildOutputFile;
-    }
-
-    return wxString();
+    return buildOutputFile;
 
 }
 // ----------------------------------------------------------------------------
