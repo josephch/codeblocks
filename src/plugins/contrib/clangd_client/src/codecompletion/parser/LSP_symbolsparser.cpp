6fe9547952013a8fb93f87641699e780cd2db36d
Christo Joseph <1538412+josephch@users.noreply.github.com>
2024-12-13 22:27:58 +0530

    clangd_client  nrvo fixes

--- src/plugins/contrib/clangd_client/src/codecompletion/parser/LSP_symbolsparser.cpp
+++ src/plugins/contrib/clangd_client/src/codecompletion/parser/LSP_symbolsparser.cpp
@@ -1109,7 +1109,7 @@ wxString LSP_SymbolsParser::DoGetDocumentSymbolFunctionArgs(const wxString& deta
     if (not detail.Length()) return args;
     if (not detail.EndsWith(')'))
     {
-        return wxString();
+        return args;
     }
     // There must be only arguments or arguments following the type
     // Find the opening paren to arguments and remove arguments to get type only
@@ -1117,10 +1117,10 @@ wxString LSP_SymbolsParser::DoGetDocumentSymbolFunctionArgs(const wxString& deta
     if (wxFound(openingParen))
     {
         args = detail.SubString(openingParen,detail.Length()-1);
-        return args.Trim(true).Trim(false);
+        args.Trim(true).Trim(false);
     }
 
-    return wxString();
+    return args;
 }
 // ----------------------------------------------------------------------------
 int LSP_SymbolsParser::FindOpeningEnclosureChar(const wxString source, int index)
@@ -1193,7 +1193,8 @@ wxString LSP_SymbolsParser::GetFullTypeFromDetail(const wxString& detail)
     if (not detail.EndsWith(')'))
     {
         fullType = detail;
-        return fullType.Trim(true).Trim(false);
+        fullType.Trim(true).Trim(false);
+        return fullType;
     }
     // There must be only arguments or arguments following the type
     // Find the opening paren to arguments and remove arguments to get type only
@@ -1201,9 +1202,9 @@ wxString LSP_SymbolsParser::GetFullTypeFromDetail(const wxString& detail)
     if (wxFound(openingParen))
     {
         fullType = detail.Mid(0,openingParen);
-        return fullType.Trim(true).Trim(false);
+        fullType.Trim(true).Trim(false);
     }
-    return wxString();
+    return fullType;
 }
 // ----------------------------------------------------------------------------
 Token* LSP_SymbolsParser::TokenExists(const wxString& name, const Token* parent, short int kindMask)
