76ba686105c945020cb0c5a8ca70af56ac2f10b6
Christo Joseph <1538412+josephch@users.noreply.github.com>
2025-12-14 14:29:46 +0530

    clangd_client : For files opened on jump to definition, allow jump to definition even if the file is not part of project, such as a system header file included by a file in project.

--- src/plugins/contrib/clangd_client/src/LSPclient/client.cpp
+++ src/plugins/contrib/clangd_client/src/LSPclient/client.cpp
@@ -2418,7 +2418,7 @@ void ProcessLanguageClient::LSP_GoToDefinition(cbEditor* pcbEd, int argCaretPosi
         return ;
     }
 
-    if (not GetLSP_IsEditorParsed(pcbEd))
+    if (not GetLSP_IsEditorParsed(pcbEd) && not m_pParser->GetParseManager()->EditorBelongsToProxyProject(pcbEd))
     {
         wxString msg = wxString::Format(_("%s\nnot yet parsed.\nProject:"),
                 wxFileName(pcbEd->GetFilename()).GetFullName());
@@ -2507,7 +2507,7 @@ void ProcessLanguageClient::LSP_GoToDeclaration(cbEditor* pcbEd, int argCaretPos
         cbMessageBox(_("LSP: attempt to LSP_GoToDeclaration before initialization."));
         return ;
     }
-    if (not GetLSP_IsEditorParsed(pcbEd))
+    if (not GetLSP_IsEditorParsed(pcbEd) && not m_pParser->GetParseManager()->EditorBelongsToProxyProject(pcbEd))
     {
         wxString msg = wxString::Format(_("%s\nnot yet parsed.\nProject:"),
                 wxFileName(pcbEd->GetFilename()).GetFullName());
