a42af8e2a6c29d3e652a6579b1aaace8114e4019
Christo Joseph <1538412+josephch@users.noreply.github.com>
2026-03-15 23:08:10 +0530

    split c and c++ lexers, use c lexer for .c extension For .h extension, keep it in C/C++ lexer by default, use C lexer if identified as part of a C only project.

--- src/sdk/cbproject.cpp
+++ src/sdk/cbproject.cpp
@@ -804,7 +804,13 @@ ProjectFile* cbProject::AddFile(int targetIndex, const wxString& filename, bool 
     else if (platform::windows && ext.IsSameAs(FileFilters::RESOURCE_EXT))
         pf->compilerVar = "WINDRES";
     else
+    {
         pf->compilerVar = "CPP"; // default
+        if (!ext.IsSameAs(FileFilters::H_EXT, false))
+        {
+            m_HasCppFiles = true;
+        }
+    }
 
     if (!m_Targets.GetCount())
     {
