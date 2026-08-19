b340975999b5c9cd4b8cc596e993ae611c73f923
Christo Joseph <1538412+josephch@users.noreply.github.com>
2026-08-19 23:16:35 +0530

    * Editor Settings: Add option to support new tab's position to be last or right to the current tab

--- src/src/editorconfigurationdlg.cpp
+++ src/src/editorconfigurationdlg.cpp
@@ -155,6 +155,7 @@ EditorConfigurationDlg::EditorConfigurationDlg(wxWindow* parent)
     XRCCTRL(*this, "cmbViewWS",                   wxChoice)->SetSelection(cfg->ReadInt("/view_whitespace",           0));
     XRCCTRL(*this, "spnSpaceExtraAscent",         wxSpinCtrl)->SetValue(cfg->ReadInt("/space_extra_ascent",          0));
     XRCCTRL(*this, "cmbCaretBuffer",              wxChoice)->SetSelection(cfg->ReadInt("/caret_buffer",             2));
+    XRCCTRL(*this, "cmbNewTabPosition",           wxChoice)->SetSelection(cfg->ReadInt("/new_tab_position",          0));
 
     // chkSpacesAroundBraces must be enabled only when chkSmartIndent is checked
     const bool smartIndentEnabled = XRCCTRL(*this, "chkSmartIndent", wxCheckBox)->GetValue();
@@ -1108,6 +1109,7 @@ void EditorConfigurationDlg::EndModal(int retCode)
         cfg->Write("/view_whitespace",                     XRCCTRL(*this, "cmbViewWS",                       wxChoice)->GetSelection());
         cfg->Write("/space_extra_ascent",                  XRCCTRL(*this, "spnSpaceExtraAscent",             wxSpinCtrl)->GetValue());
         cfg->Write("/caret_buffer", XRCCTRL(*this, "cmbCaretBuffer", wxChoice)->GetSelection());
+        cfg->Write("/new_tab_position",                    XRCCTRL(*this, "cmbNewTabPosition",               wxChoice)->GetSelection());
 
 #if defined(__WXMSW__) && wxCHECK_VERSION(3, 1, 0)
         cfg->Write("/technology",                          XRCCTRL(*this, "cmbTechnology",                   wxChoice)->GetSelection());
